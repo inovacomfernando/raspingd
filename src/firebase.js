8383ec947b4ca8b09473f70dc888bd6d2e364141
inovacomfernando <fernandoluizsouzaramalho@gmail.com>
2025-05-24 23:49:39 -0300

    arquivo firebase>>src "inicializa sdk do projeto"

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCO5NwIrZjYcC2j0yhz8PUsBRj4F7goObM",
+  authDomain: "marketwise-scraper.firebaseapp.com",
+  projectId: "marketwise-scraper",
+  storageBucket: "marketwise-scraper.firebasestorage.app",
+  messagingSenderId: "254450404570",
+  appId: "1:254450404570:web:a642610cd4935294dde9e3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
